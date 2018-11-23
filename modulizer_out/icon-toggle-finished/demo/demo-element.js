c259049b0fd13c40d4fcc3cf15fdcf5ce513cd5d
Thiago Outeiro <topd97@dcc.ufrj.br>
2018-11-23 14:11:34 -0200

    depois do modulizer

---
+++ modulizer_out/icon-toggle-finished/demo/demo-element.js
@@ -0,0 +1,43 @@
+import '../icon-toggle.js';
+import { html } from '../../../../@polymer/polymer/lib/utils/html-tag.js';
+import { Element } from '../../../../@polymer/polymer/polymer-element.js';
+class DemoElement extends Element {
+  static get template() {
+    return html`
+    <style>
+      :host {
+        --icon-toggle-color: lightgrey;
+        --icon-toggle-outline-color: black;
+        --icon-toggle-pressed-color: red;        
+        font-family: sans-serif;
+      };
+    </style>
+  
+    <h3>Statically-configured icon-toggles</h3>
+    
+    <icon-toggle toggle-icon="star"></icon-toggle>
+    <icon-toggle toggle-icon="star" pressed=""></icon-toggle>
+    
+    <h3>Data-bound icon-toggle</h3>
+
+    <!-- use a computed binding to generate the message -->
+    <div><span>[[_message(isFav)]]</span></div>
+
+    <!-- curly brackets ({{}}} allow two-way binding --> 
+    <icon-toggle toggle-icon="favorite" pressed="{{isFav}}"></icon-toggle>
+`;
+  }
+
+  static get is() { return "demo-element" }
+
+  _message(fav) {
+    if (fav) {
+      return "You really like me!";
+    } 
+    else {
+      return "Do you like me?";
+    }
+  }
+}
+
+customElements.define(DemoElement.is, DemoElement);
